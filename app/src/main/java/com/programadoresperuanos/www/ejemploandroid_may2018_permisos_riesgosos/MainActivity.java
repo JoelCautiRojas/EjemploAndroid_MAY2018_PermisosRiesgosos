1779238ba1de98cadd7f1924f9b0684b7c506c4a
Joel Cauti Rojas <joel.cauti@gmail.com>
2018-05-24 19:17:13 -0500

    primer commit
    primer commit

---
+++ app/src/main/java/com/programadoresperuanos/www/ejemploandroid_may2018_permisos_riesgosos/MainActivity.java
@@ -0,0 +1,47 @@
+package com.programadoresperuanos.www.ejemploandroid_may2018_permisos_riesgosos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button boton1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton1 = findViewById(R.id.button);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mimenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opcion1:
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                return true;
+            case R.id.opcion2:
+                startActivity(new Intent(getApplicationContext(),Main4Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
